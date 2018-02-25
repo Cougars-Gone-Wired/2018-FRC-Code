1eeee181e7de7ae226297a85335498b8f43791fc
tiegu <tiegu@192.168.0.9>
2018-02-24 19:54:50 -0700

    I don't even know

--- 2018ReorganizedCode/src/org/usfirst/frc/team2996/robot/RobotLogger.java
+++ 2018ReorganizedCode/src/org/usfirst/frc/team2996/robot/RobotLogger.java
@@ -22,6 +22,7 @@ public class RobotLogger extends Object implements Runnable {
 	boolean autonomousState = false;
 	boolean teleopState = false;
 	boolean shooterState = false;
+<<<<<<< Upstream, based on origin/master
 	boolean loggingActive;
 	boolean enabled;
 	boolean titleRun = false;
@@ -226,6 +227,125 @@ public class RobotLogger extends Object implements Runnable {
 	
 
 	
+=======
+	Logger logging;
+
+	public Logger createLogger() throws SecurityException, IOException {
+		Logger logger = Logger.getLogger(RobotLogger.class.getName());
+		logger.setUseParentHandlers(false);
+		Level level = Level.FINE;
+		logger.setLevel(level);
+
+		File file = new File("/home/lvuser/" + "RobotLog" + dateFormat.format(new Date()) + ".csv");
+		FileHandler fh = new FileHandler(file.getAbsolutePath());
+		fh.setFormatter(new RobotLoggerFormatter());
+		logger.addHandler(fh);
+
+		// File file = new File(name + format.format(new Date()) + ".csv");
+		System.out.println(file.getAbsolutePath());
+		fh.setFormatter(new RobotLoggerFormatter());
+		logger.addHandler(fh);
+		return logger;
+	}
+
+	RobotLogger(Robot robot) {
+		this.robot = robot;
+		running = true;
+	}
+
+	public void halt() {
+		System.out.println("Logging Halted");
+		running = false;
+	}
+
+	public void run() {
+		System.out.println("Logging Started");
+		while (running) {
+			boolean loggingActive = SmartDashboard.getBoolean("logging", false);
+			boolean enabled = robot.isEnabled();
+			if (loggingActive && enabled) {
+				try {
+					if (logging == null) {
+						logging = createLogger();
+						writeHeader();
+					}
+					// PIDlog();
+					movementLog();
+				} catch (Throwable e) {
+					// TODO Auto-generated catch block
+					// e.printStackTrace();
+				}
+
+			} else {
+				autonomousState = false;
+				teleopState = false;
+			}
+			// 20 milliseconds
+			Timer.delay(0.02);
+		}
+	}
+
+	public void writeHeader() throws Throwable {
+
+		logging.fine(", FrontLeftMotor, FrontRightMotor, RearLeftMotor, RearRightMotor");
+		logging.fine(", Double, Double, Double, Double");
+	}
+
+	public void movementLog() throws Throwable {
+		StringBuilder sb = new StringBuilder();
+		robot.getDrive().getFrontLeftMotor();
+		if (robot.getDrive() != null) {
+			if (robot.getDrive().getFrontLeftSensors() != null) {
+				sb.append(DELI).append(robot.getDrive().getFrontLeftSensors().getQuadraturePosition());
+			} else {
+				sb.append(DELI).append("1324");
+			}
+			if (robot.getDrive().getFrontRightSensors() != null) {
+				sb.append(DELI).append(robot.getDrive().getFrontRightSensors().getQuadraturePosition());
+			} else {
+				sb.append(DELI).append("1324");
+			}
+			if (robot.getDrive().getRearLeftSensors() != null) {
+				sb.append(DELI).append(robot.getDrive().getRearLeftSensors().getQuadraturePosition());
+			} else {
+				sb.append(DELI).append("1324");
+			}
+			if (robot.getDrive().getRearRightSensors() != null) {
+				sb.append(DELI).append(robot.getDrive().getRearRightSensors().getQuadraturePosition());
+			} else {
+				sb.append(DELI).append("1324");
+			}
+		}
+		// if (robot.isAutonomous()) {
+		// if (!autonomousState) {
+		// logging.fine("Auto Begin");
+		// autonomousState = true;
+		// }
+		//
+		// if (autonomousState) {
+		// logging.fine("");
+		// }
+		//
+		// } else {
+		// autonomousState = false;
+		// }
+		//
+		// if (robot.isOperatorControl()) {
+		// if (!teleopState) {
+		// logging.fine("Teleop Begin");
+		// teleopState = true;
+		// }
+		//
+		// if (teleopState) {
+		// logging.fine("");
+		// }
+		//
+		// } else {
+		// teleopState = false;
+		// }
+		logging.fine(sb.toString());
+	}
+>>>>>>> 3c023c5 Add files via upload
 
 	public static String findType(String s) {
 		return "String";
